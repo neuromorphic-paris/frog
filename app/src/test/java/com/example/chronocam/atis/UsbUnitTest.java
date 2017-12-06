90580ba75180b24ee7e18cfb037ff372721f366a
Gregor Lenz <lenz.gregor@gmail.com>
2017-12-06 16:29:01 +0100

    using Robolectronic on top of Mockito for Unit tests now

--- app/src/test/java/com/example/chronocam/atis/UsbUnitTest.java
+++ app/src/test/java/com/example/chronocam/atis/UsbUnitTest.java
@@ -1,68 +1,83 @@
 package com.example.chronocam.atis;
 
-import android.content.Context;
+import android.content.Intent;
 import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbManager;
-import android.util.Log;
 
+import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
 import java.util.HashMap;
 
-import static org.mockito.Mockito.when;
+import static junit.framework.Assert.*;
+import static org.mockito.Mockito.*;
 
 /**
  * Created by gregorlenz on 05/12/17.
  */
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
 public class UsbUnitTest {
+    @Rule
+    public MockitoRule rule = MockitoJUnit.rule();
     @Mock
     UsbManager usbManager;
     @Mock
     UsbDevice usbDevice = Mockito.mock(UsbDevice.class);
-
     @InjectMocks
-    MainActivity mainActivity = new MainActivity();
+    MainActivity mainActivity = Robolectric.setupActivity(MainActivity.class);
 
     HashMap<String, UsbDevice> deviceList = new HashMap<>();
 
     @Before
     public void setup() {
-        //mainActivity = new MainActivity();
-        Mockito.mock(Log.class);
         deviceList.put("testDevice", usbDevice);
     }
 
     @Test
-    public void testTest() {
-        assert mainActivity.checkNumber(3);
-    }
-
-    @Test
-    public void testGetUsbDevice() {
+    public void getUsbDevice_returnsUsbDevice() {
         when(usbManager.getDeviceList()).thenReturn(deviceList);
         UsbDevice result = mainActivity.getUsbDevice();
-        assert result.equals(usbDevice);
+        assertEquals(result, usbDevice);
     }
 
     @Test
-    public void testUsbDeviceHasPermissions() {
+    public void checkPermissions_validDevice_returnsTrue() {
         when(usbManager.hasPermission(usbDevice)).thenReturn(true);
         boolean result = mainActivity.checkPermissions(usbDevice);
-        assert result;
+        assertTrue(result);
     }
 
     @Test
-    public void testUsbDeviceDoesNotHavePermission() {
-        when(usbManager.hasPermission(usbDevice)).thenReturn(true);
+    public void checkPermissions_invalidDevice_returnsFalse() {
+        when(usbManager.hasPermission(usbDevice)).thenReturn(false);
         boolean result = mainActivity.checkPermissions(usbDevice);
-        assert !result;
+        assertFalse(result);
+    }
+
+    @Test
+    public void checkPermissions_nullDevice_returnsFalse(){
+        boolean result = mainActivity.checkPermissions(null);
+        assertFalse(result);
+    }
+
+    @Test
+    public void checkPermissions_nullUsbManager_returnsFalse(){
+        usbManager = null;
+        boolean result = mainActivity.checkPermissions(usbDevice);
+        assertFalse(result);
     }
 }

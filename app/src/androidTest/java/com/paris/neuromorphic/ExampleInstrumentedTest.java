4d79912d6bceabcfbe3b39307a5f2d3494934da0
Gregor Lenz <mail@lenzgregor.com>
2019-02-28 15:01:39 +0100

    extend instrumentation test, add dep for Paper

--- app/src/androidTest/java/com/paris/neuromorphic/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/paris/neuromorphic/ExampleInstrumentedTest.java
@@ -2,11 +2,20 @@ package com.paris.neuromorphic;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.RequiresDevice;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static junit.framework.Assert.assertEquals;
 import static org.junit.Assert.*;
 
 /**
@@ -16,11 +25,24 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> rule  = new ActivityTestRule<MainActivity>(MainActivity.class);
+
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
+        assertEquals("com.vision.neuromorphic.frog", appContext.getPackageName());
+    }
 
-        assertEquals("com.example.chronocam.atis", appContext.getPackageName());
+    @RequiresDevice
+    @Test
+    public void testStartUp() throws Exception {
+        //MainActivity activity = rule.getActivity();
+        onView(withId(R.id.camera_preview)).check(matches((isDisplayed())));
+        //View testView = activity.findViewById(R.id.camera_preview);
+        //assertNotNull(testView);
     }
 }
